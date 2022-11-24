8ee0f2e2b7829cb2b57461c363595b636d2d498e
dev-parthive <dev-parthive>
2022-11-24 09:28:05 +0600

    react query

--- src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,20 +1,44 @@
+import { async } from '@firebase/util';
+import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointment = ({selectedDate}) => {
-    const [appointmentOptions, setAppointmentOptions] = useState([])
+    // const [appointmentOptions, setAppointmentOptions] = useState([])
     const [treatment, setTreatment] = useState(null)
 
-    useEffect( ()=>{
-        fetch('appointemnetOption.json')
-        .then(res=> res.json())
-        .then(data =>{
-            console.log(data)
-            setAppointmentOptions(data)
-        })
-    },[] )
+// react query use korate amader useEffect and useState kono ta use kora lage nai 
+
+    // const {data:appointmentOptions = []} = useQuery({
+    //     queryKey: ['appointmentOption'], 
+    //     queryFn: () => fetch('http://localhost:5000/appointmentOption') 
+    //     .then(res => res.json())
+     
+    // })
+
+
+    const {data:appointmentOptions = []}  = useQuery({
+        queryKey: ['appointmentOptions'], 
+        queryFn: async () =>{
+            const res = await fetch('http://localhost:5000/appointmentOption')
+            const data = await res.json()
+            return data
+        }
+    })
+ 
+
+
+
+    // useEffect( ()=>{
+    //     fetch('http://localhost:5000/appointmentOption')
+    //     .then(res=> res.json())
+    //     .then(data =>{
+    //         console.log(data)
+    //         setAppointmentOptions(data)
+    //     })
+    // },[] )
     return (
         <section className='mt-16'>
             <p className='text-center text-secondary font-bold'>Available Appointment on {format(selectedDate, 'PP')}</p>
